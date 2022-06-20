f502fb6fcf0fdd64923257298b3ba86e648c836a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-06-20 16:55:45 -0700

    - basic layout for Signup Screen

--- app/src/main/java/com/example/myinstaclone/presentation/signupscreen/SignUpScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/signupscreen/SignUpScreen.kt
@@ -1,14 +1,97 @@
 package com.example.myinstaclone.presentation.signupscreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.myinstaclone.R
 import com.example.myinstaclone.presentation.IgViewModel
 
 @Composable
 fun SignupScreen(
     navController: NavController,
     vm: IgViewModel
-){
-    Text(text = "Sign Up Screen")
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .verticalScroll(
+                    rememberScrollState()
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val usernameState = remember {
+                mutableStateOf(TextFieldValue())
+            }
+            val emailState = remember {
+                mutableStateOf(TextFieldValue())
+            }
+            val passState = remember {
+                mutableStateOf(TextFieldValue())
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.ig_logo),
+                contentDescription = "Instagram Logo",
+                modifier = Modifier
+                    .width(250.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "Signup",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+
+            )
+            OutlinedTextField(
+                value = usernameState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") }
+            )
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Email") }
+            )
+            OutlinedTextField(
+                value = passState.value,
+                onValueChange = { passState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Signup")
+            }
+            Text(text = "Already a user? Go to login ->",
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { }
+            )
+        }
+    }
 }
