339d5782b235daab72999d6054ba3d8434c4abab
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-06-21 17:15:04 -0700

    - added all the UI elements for the LoginScreen.kt - updated SignUpScreen.kt to clear the focus when signup button is clicked to dismiss the keyboard

--- app/src/main/java/com/example/myinstaclone/presentation/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/loginscreen/LoginScreen.kt
@@ -1,25 +1,94 @@
 package com.example.myinstaclone.presentation.loginscreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.myinstaclone.R
+import com.example.myinstaclone.presentation.CommonProgressSpinner
 import com.example.myinstaclone.presentation.IgViewModel
 import com.example.myinstaclone.presentation.ScreenDestination
 import com.example.myinstaclone.presentation.navigateTo
 
 @Composable
 fun LoginScreen(navController: NavController, vm: IgViewModel) {
-    Text(text = "New here? Go to signup ->",
-        color = Color.Blue,
-        modifier = Modifier
-            .padding(8.dp)
-            .clickable {
-                navigateTo(navController, ScreenDestination.Signup)
+    val focus = LocalFocusManager.current
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .verticalScroll(
+                    rememberScrollState()
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val emailState = remember { mutableStateOf(TextFieldValue()) }
+            val passState = remember { mutableStateOf(TextFieldValue()) }
+
+            Image(
+                painter = painterResource(id = R.drawable.ig_logo),
+                contentDescription = "Instagram Logo",
+                modifier = Modifier
+                    .width(250.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "Login",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+
+            )
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Email") }
+            )
+            OutlinedTextField(
+                value = passState.value,
+                onValueChange = { passState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(onClick = {
+                focus.clearFocus(force = true)
+            }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Login")
             }
-    )
+            Text(text = "New here? Go to signup ->",
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable {
+                        navigateTo(navController, ScreenDestination.Signup)
+                    }
+            )
+        }
+        val isLoading = vm.inProgress.value
+        if (isLoading) {
+            CommonProgressSpinner()
+        }
+    }
 }
--- app/src/main/java/com/example/myinstaclone/presentation/signupscreen/SignUpScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/signupscreen/SignUpScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -32,6 +33,7 @@ fun SignupScreen(
     navController: NavController,
     vm: IgViewModel
 ) {
+    val focus = LocalFocusManager.current
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
@@ -87,6 +89,7 @@ fun SignupScreen(
                 visualTransformation = PasswordVisualTransformation()
             )
             Button(onClick = {
+                focus.clearFocus(force = true)
                 vm.onSignup(
                     usernameState.value.text,
                     emailState.value.text,
