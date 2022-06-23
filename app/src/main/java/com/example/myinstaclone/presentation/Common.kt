c455f79f225a18fecc2a44d62e8119c2d734b803
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-06-22 17:13:44 -0700

    - added icons for generic user profile, and plus sign
    - added common composable for displaying generic image
    -- uses coil library to load image from url
    -- will display progress spinner while coil is loading image
    
    - added common composable for displaying a user image card
    -- display generic profile image if there is no image url to load from
    -- otherwise display provided image via url
    
    - implemneted ProfileImage composable in MyPostsScreen.kt
    -- displays circular profile picture
    -- display a "+" icon for user to click to add a new post

--- app/src/main/java/com/example/myinstaclone/presentation/Common.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/Common.kt
@@ -1,11 +1,12 @@
 package com.example.myinstaclone.presentation
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -14,8 +15,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import com.example.myinstaclone.R
 
 @Composable
 fun NotificationMessage(vm: IgViewModel) {
@@ -57,6 +66,47 @@ fun CheckSignedIn(navController: NavController, vm: IgViewModel) {
         alreadyLoggedIn.value = true
         navController.navigate(ScreenDestination.Feed.route) {
             popUpTo(0)
+        }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun CommonImage(
+    data: String?,
+    modifier: Modifier = Modifier.wrapContentSize(),
+    contentScale: ContentScale = ContentScale.Crop
+) {
+    val painter = rememberImagePainter(data = data)
+
+    if (painter.state is ImagePainter.State.Loading) {
+        CommonProgressSpinner()
+    } else {
+        Image(
+            painter = painter,
+            contentDescription = "Profile Picture",
+            modifier = modifier,
+            contentScale = contentScale
+        )
+    }
+}
+
+@Composable
+fun UserImageCard(
+    userImage: String?,
+    modifier: Modifier = Modifier
+        .padding(8.dp)
+        .size(64.dp)
+) {
+    Card(shape = CircleShape, modifier = modifier) {
+        if (userImage.isNullOrBlank()) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user),
+                contentDescription = "Profile Picture",
+                colorFilter = ColorFilter.tint(Color.Gray)
+            )
+        } else {
+            CommonImage(data = userImage)
         }
     }
 }
--- app/src/main/java/com/example/myinstaclone/presentation/mypostsscreen/MyPostsScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/mypostsscreen/MyPostsScreen.kt
@@ -1,19 +1,29 @@
 package com.example.myinstaclone.presentation.mypostsscreen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.myinstaclone.R
 import com.example.myinstaclone.presentation.IgViewModel
+import com.example.myinstaclone.presentation.UserImageCard
 import com.example.myinstaclone.presentation.bottomnavigationmenu.BottomNavigationItem
 import com.example.myinstaclone.presentation.bottomnavigationmenu.BottomNavigationMenu
 
@@ -84,5 +94,29 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
 
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
+    Box(modifier = Modifier
+        .padding(top = 16.dp)
+        .clickable { onClick.invoke() }) {
+        UserImageCard(
+            userImage = imageUrl, modifier = Modifier
+                .padding(8.dp)
+                .size(80.dp)
+        )
 
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.White),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = "Add",
+                modifier = Modifier.background(Color.Blue),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+        }
+    }
 }
