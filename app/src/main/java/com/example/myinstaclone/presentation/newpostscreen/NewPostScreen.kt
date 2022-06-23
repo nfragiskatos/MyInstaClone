df98689c526a5882187eeac8e801c91a5c875af0
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-06-23 16:38:49 -0700

    - UI for NewPostScreen.kt

--- app/src/main/java/com/example/myinstaclone/presentation/newpostscreen/NewPostScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/newpostscreen/NewPostScreen.kt
@@ -1,10 +1,81 @@
 package com.example.myinstaclone.presentation.newpostscreen
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.myinstaclone.presentation.CommonDivider
+import com.example.myinstaclone.presentation.CommonProgressSpinner
 import com.example.myinstaclone.presentation.IgViewModel
 
 @Composable
 fun NewPostScreen(navController: NavController, vm: IgViewModel, encodedUri: String) {
 
+    val imageUri by remember { mutableStateOf(encodedUri) }
+    var description by rememberSaveable() { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Cancel", modifier = Modifier.clickable { navController.popBackStack() })
+            Text(text = "Post", modifier = Modifier.clickable {
+                focusManager.clearFocus()
+                //call the view model function
+            })
+        }
+
+        CommonDivider()
+
+        Image(
+            painter = rememberImagePainter(imageUri),
+            contentDescription = "post image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 150.dp),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Row(modifier = Modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                label = { Text(text = "Description") },
+                singleLine = false,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                )
+            )
+        }
+    }
+    val inProgress = vm.inProgress.value
+    if (inProgress) {
+        CommonProgressSpinner()
+    }
 }
