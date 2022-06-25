2a586cb85f4b1a15e3894d81cdbbff514fc6e23b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-06-25 11:03:07 -0700

    - added composable to display a grid of current user's posts

--- app/src/main/java/com/example/myinstaclone/presentation/mypostsscreen/MyPostsScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/mypostsscreen/MyPostsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
@@ -19,15 +21,34 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.myinstaclone.R
+import com.example.myinstaclone.data.remote.dto.PostDto
 import com.example.myinstaclone.presentation.*
 import com.example.myinstaclone.presentation.bottomnavigationmenu.BottomNavigationItem
 import com.example.myinstaclone.presentation.bottomnavigationmenu.BottomNavigationMenu
+
+data class PostRow(
+    var post1: PostDto? = null,
+    var post2: PostDto? = null,
+    var post3: PostDto? = null,
+) {
+    fun isFull(): Boolean = post1 != null && post2 != null && post3 != null
+    fun add(post: PostDto) {
+        if (post1 == null) {
+            post1 = post
+        } else if (post2 == null) {
+            post2 = post
+        } else if (post3 == null) {
+            post3 = post
+        }
+    }
+}
 
 @Composable
 fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
@@ -45,6 +66,9 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
 
     val userData = vm.userData.value
     val isLoading = vm.inProgress.value
+
+    val postsLoading = vm.refreshPostsProgress.value
+    val posts = vm.posts.value
 
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -95,9 +119,13 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
             ) {
                 Text(text = "Edit Profile", color = Color.Black)
             }
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Posts List")
-            }
+            PostList(
+                isContextLoading = isLoading,
+                postsLoading = postsLoading,
+                posts = posts,
+                modifier = Modifier.weight(1f).padding(1.dp).fillMaxSize(),
+
+                ) {}
 
         }
         BottomNavigationMenu(
@@ -137,5 +165,93 @@ private fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
                 colorFilter = ColorFilter.tint(Color.White)
             )
         }
+    }
+}
+
+@Composable
+fun PostList(
+    isContextLoading: Boolean,
+    postsLoading: Boolean,
+    posts: List<PostDto>,
+    modifier: Modifier,
+    onPostClick: (PostDto) -> Unit
+) {
+    if (postsLoading) {
+        CommonProgressSpinner()
+    } else if (posts.isEmpty()) {
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (!isContextLoading) {
+                Text(text = "No posts available")
+            }
+        }
+    } else {
+        LazyColumn(modifier = modifier) {
+            val rows = arrayListOf<PostRow>()
+            var currentRow = PostRow()
+            rows.add(currentRow)
+
+            for (post in posts) {
+                if (currentRow.isFull()) {
+                    currentRow = PostRow()
+                    rows.add(currentRow)
+                }
+                currentRow.add(post = post)
+            }
+            items(items = rows) { row ->
+                PostRowDisplay(item = row, onPostClick = onPostClick)
+            }
+        }
+    }
+}
+
+@Composable
+fun PostRowDisplay(item: PostRow, onPostClick: (PostDto) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+    ) {
+        PostImage(imageUrl = item.post1?.postImage, modifier = Modifier
+            .weight(1f)
+            .clickable {
+                item.post1?.let { post ->
+                    onPostClick(post)
+                }
+            }
+        )
+        PostImage(imageUrl = item.post2?.postImage, modifier = Modifier
+            .weight(1f)
+            .clickable {
+                item.post2?.let { post ->
+                    onPostClick(post)
+                }
+            }
+        )
+        PostImage(imageUrl = item.post3?.postImage, modifier = Modifier
+            .weight(1f)
+            .clickable {
+                item.post3?.let { post ->
+                    onPostClick(post)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun PostImage(imageUrl: String?, modifier: Modifier) {
+    Box(modifier = modifier) {
+        var imageModifier = Modifier
+            .padding(1.dp)
+            .fillMaxSize()
+        if (imageUrl == null) {
+            imageModifier = imageModifier.clickable(enabled = false) {}
+        }
+
+        CommonImage(data = imageUrl, modifier = imageModifier, contentScale = ContentScale.Crop)
     }
 }
