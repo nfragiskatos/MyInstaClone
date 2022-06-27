68e2ba27a1d20a0002118afb6b3ab9210c1b2677
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-06-26 18:21:18 -0700

    - finished implementing UI for CommentsScreen.kt to display the comments associated with the post

--- app/src/main/java/com/example/myinstaclone/presentation/commentsscreen/CommentsScreen.kt
+++ app/src/main/java/com/example/myinstaclone/presentation/commentsscreen/CommentsScreen.kt
@@ -2,6 +2,8 @@ package com.example.myinstaclone.presentation.commentsscreen
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -11,11 +13,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.myinstaclone.data.remote.dto.CommentDto
+import com.example.myinstaclone.presentation.CommonProgressSpinner
 import com.example.myinstaclone.presentation.IgViewModel
 
 @Composable
@@ -30,9 +36,34 @@ fun CommentsScreen(
 
     val focusManager = LocalFocusManager.current
 
+    val comments = vm.comments.value
+    val commentsProgress = vm.commentsProgress.value
+
     Column(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.weight(1f)) {
-            Text(text = "Comments")
+            if (commentsProgress) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CommonProgressSpinner()
+                }
+            } else if (comments.isEmpty()) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "No comments available")
+                }
+            } else {
+                LazyColumn(modifier = Modifier.weight(1f)) {
+                    items(items = comments) { comment ->
+                        CommentRow(comment = comment)
+                    }
+                }
+            }
         }
 
         Row(
@@ -64,5 +95,16 @@ fun CommentsScreen(
             }
         }
     }
+}
 
+@Composable
+fun CommentRow(comment: CommentDto) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(text = comment.username ?: "", fontWeight = FontWeight.Bold)
+        Text(text = comment.text ?: "", modifier = Modifier.padding(start = 8.dp))
+    }
 }
